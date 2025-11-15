870261251166ce6216a150fdbcd85fdadd47b86c
Pranay <gadhpranay@gmail.com>
2025-11-15 14:20:01 +0530

    basic update and delete commands

---
+++ update and delete.sql
@@ -0,0 +1,20 @@
+USE test_db;
+
+UPDATE student
+SET subject = 'Math'
+WHERE subject = 'Mathematics';
+
+UPDATE student 
+SET name = 'Anshita', subject = 'Accounts'
+WHERE roll_no = 2;
+
+UPDATE student
+SET name = 'human';
+
+DELETE FROM student
+WHERE subject = 'Maths' OR subject = 'Math' OR subject = 'Not Decided';
+
+DELETE FROM student;
+
+DESCRIBE student;
+SELECT * FROM student;
