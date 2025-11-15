8dc1f11a9066905771a456fdbecfdcda850e0263
Pranay <gadhpranay@gmail.com>
2025-11-15 13:16:44 +0530

    datatypes intro
    datatypes intro

---
+++ datatypes.sql
@@ -0,0 +1,9 @@
+create database test_db;
+-- datatypes
+int  -- whole numbers
+decimal(m,n)  -- decimal points m: digits for a number, n: digits after decimal place
+varchar (l) -- strings of length l
+blob -- binary large object, used for large data (binary)
+date  -- yyyy-mm-dd
+timestamp  -- yyyy-mm-dd hh-mm-ss
+
