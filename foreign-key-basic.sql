435bc986655411478f5fd0e9a6565a75a7ec8765
Pranay <gadhpranay@gmail.com>
2025-11-15 16:02:51 +0530

    just a very basic foreign key constraint

---
+++ foreign-key-basic.sql
@@ -0,0 +1,16 @@
+USE test_db;
+
+CREATE TABLE teacher(
+teacher_id INT PRIMARY KEY,
+name VARCHAR(20) NOT NULL,
+dept CHAR(4),
+student_id INT,
+FOREIGN KEY (student_id) REFERENCES student(roll_no)
+);
+
+INSERT INTO teacher(teacher_id, name, dept) VALUES
+(101, 'abc', 'mat'),
+(102, 'abd','sci');
+
+SELECT * FROM teacher;
+SELECT * FROM student;
