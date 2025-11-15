2db1b1aa34b073cc6290c60d702f9ac91c830530
Pranay <gadhpranay@gmail.com>
2025-11-15 14:37:16 +0530

    Add additional queries to basic-queries.sql

--- basic-queries.sql
+++ basic-queries.sql
@@ -3,3 +3,11 @@ SELECT *
 FROM student
 ORDER BY name DESC
 LIMIT 4;
+
+SELECT name, subject
+FROM student
+WHERE subject  = 'Not Decided' OR name = 'Pranay';
+
+SELECT *
+FROM student
+WHERE subject <> 'Mathematics' AND subject <> 'Maths';
