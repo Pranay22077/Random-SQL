601ffec9c9122997157efe7a3e6a06c648df74b3
Pranay <gadhpranay@gmail.com>
2025-11-17 02:01:28 +0530

    Update aggregate-functions.sql

--- aggregate-functions.sql
+++ aggregate-functions.sql
@@ -1,1 +1,8 @@
 -- Aggregate functions
+COUNT --counts all rows in table (skips missing values)
+COUNT (*) -- counts all rows, regardless of NULL values
+MAX -- maximun of all rows
+MIN -- minimum of all rows
+AVG -- average 
+SUM -- sum of all entries in that column
+-- AVG and SUM only work on numerical data
