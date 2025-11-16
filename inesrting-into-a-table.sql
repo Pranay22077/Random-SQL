1ac419039b51b7bfdcde08975d2a03334b33aa32
Pranay <gadhpranay@gmail.com>
2025-11-16 22:33:02 +0530

    Optimize student table insertions
    Consolidated multiple INSERT statements into a single statement for better efficiency.

--- inesrting-into-a-table.sql
+++ inesrting-into-a-table.sql
@@ -1,12 +1,9 @@
 USE test_db;
 
-SELECT * FROM student;
-
 INSERT INTO student VALUES(
-1, 'Pranay', 'Mathematics'
-);
-INSERT INTO student VALUES(
-2, 'Pruunay', 'CSE'
-);
+(1, 'Pranay', 'Mathematics'),
+(2, 'Pruunay', 'CSE');
 
 INSERT INTO student(roll_no, subject) VALUES( 3, 'ECE');
+
+SELECT * FROM student;
