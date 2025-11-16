e5a6c42f5a0a7f5fbd1e22291d3350d900c33120
Pranay <gadhpranay@gmail.com>
2025-11-17 01:10:39 +0530

    Refactor database schema for departments and employees
    Renamed tables and columns, added and dropped columns, and modified existing columns in employees.

--- full-script.sql
+++ full-script.sql
@@ -1,13 +1,16 @@
 USE company;
 
+-- Renaming a table
 RENAME TABLE gemini_tut.departments TO company.departments;
 
+-- Create parent table
 CREATE TABLE departments(
 dept_id CHAR(4) PRIMARY KEY,
 dept_name VARCHAR(30) NOT NULL,
 location VARCHAR(50)
 );
 
+-- create child table
 create table employees(
 emp_id int primary key,
 first_name varchar(30) not null,
@@ -18,11 +21,28 @@ dept_id char(4),
 FOREIGN KEY (dept_id) references departments(dept_id)
 );
 
-
+-- Renaming a column
 ALTER TABLE departments
 CHANGE COLUMN dept_name name VARCHAR(40);
 
 ALTER TABLE departments
+RENAME TO department;
+
+ALTER TABLE departments
 CHANGE COLUMN name dept_name VARCHAR(40);
 
-DESCRIBE departments;
+DESCRIBE department;
+
+-- adding a new column
+ALTER TABLE employees
+ADD age int not null;
+
+-- dropping a column
+ALTER TABLE employees
+DROP COLUMN age;
+
+-- modifying an already existing column
+ALTER TABLE employees
+MODIFY first_name VARCHAR(40) NOT NULL UNIQUE;
+
+describe employees;
