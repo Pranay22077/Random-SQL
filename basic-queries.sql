e1b5d7b624f4a357a82f1622d72b55ff834f0016
Pranay <gadhpranay@gmail.com>
2025-11-24 22:07:04 +0530

    Update basic-queries.sql

--- basic-queries.sql
+++ basic-queries.sql
@@ -1,13 +1,21 @@
+-- Switch to the target database
 USE test_db;
+
+-- Get the top 4 students ordered by name (descending)
 SELECT *
 FROM student
 ORDER BY name DESC
 LIMIT 4;
 
+-- Retrieve students whose subject is 'Not Decided' or whose name is 'Pranay'
 SELECT name, subject
 FROM student
-WHERE subject  = 'Not Decided' OR name = 'Pranay';
+WHERE subject = 'Not Decided'
+   OR name = 'Pranay';
 
+-- Get all students excluding those with 'Mathematics' or 'Maths' as subject
 SELECT *
 FROM student
-WHERE subject <> 'Mathematics' AND subject <> 'Maths';
+WHERE subject <> 'Mathematics'
+  AND subject <> 'Maths';
+
