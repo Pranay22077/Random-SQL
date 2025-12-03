72c2783155319c144a89a70350e64f9ce8688728
Pranay <gadhpranay@gmail.com>
2025-12-03 19:53:16 +0530

    Fix SQL insert statement for student records

--- inesrting-into-a-table.sql
+++ inesrting-into-a-table.sql
@@ -2,7 +2,7 @@ USE test_db;
 
 INSERT INTO student VALUES(
 (1, 'Pranay', 'Mathematics'),
-(2, 'Pruunay', 'CSE');
+(2, 'Rishabh', 'CSE');
 
 INSERT INTO student(roll_no, subject) VALUES( 3, 'ECE');
 
